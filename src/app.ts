4838b061aff62d5abf876ceaae78d0a45dc5e98d
Uliana Savitskaua <uljasha.sav@gmail.com>
2023-06-21 21:21:35 +0600

    feat: create cluster and add npm command to start multi

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
 import http from 'http';
 import dotenv from 'dotenv';
 import { Router } from './router';
+import { startCluster } from './cluster';
 import { processReq } from './utils/processReq';
+import { HOSTNAME } from './constants';
+
 
 dotenv.config();
-
-const hostname = '127.0.0.1';
 const port = process.env.SERVER_PORT ? +process.env.SERVER_PORT : 3000;
 
-const server = http.createServer(async (req, res) => {
-  const reqWithBody = await processReq(req, res);
-  Router.route(reqWithBody, res);
+if (process.env.NODE_ENV === 'multi') {
+  startCluster();
+} else {
+  const server = http.createServer(async (req, res) => {
+    const reqWithBody = await processReq(req, res);
+    Router.route(reqWithBody, res);
 });
 
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${ hostname }:${ port }/`);
+server.listen(port, HOSTNAME, () => {
+    console.log(`Server running at http://${ HOSTNAME }:${ port }/`);
 });
-
-server.on('error', (err: Error) => console.log(`Operation failed!\n${ err.message }`));
+}
